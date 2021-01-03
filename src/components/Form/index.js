ecaf7ece10b13cb7ca5132afab4e11c36db94e6a
Sergey Tolstokorov <stolstokorov2020@yandex.ru>
2021-01-03 20:34:40 +0700

    implement Form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { createRef, useState } from 'react';
+import { connect } from 'react-redux';
+import style from './index.module.css';
+import { addItem, reverse } from '../../redux/reducers/toDoList/actions/actionCreators'
+
 
 const Form = (props) => {
-    // this is stub 
+    const inputRef = createRef();
+    const [inputValue, setInputValue] = useState(props.placeholder);
+
     return (
         <>
-            <div style= {{ textAlign: 'center' }}>
-                <p>Form</p>
+            <div className={ style.wrapper }>
+                <form onSubmit={ e => {
+                    e.preventDefault();
+                    props.addItem(inputValue);
+                    setInputValue('');
+                } } >
+                    <div>
+                        <label>
+                            <p>Do something</p>
+                            <input
+                                type="text"
+                                ref={ inputRef }
+                                value={ inputValue }
+                                onChange={ () => {
+                                    setInputValue(inputRef.current.value)
+                                } }
+                            />
+                        </label>
+                        <button>Add</button>
+                    </div>
+                    <p><input type="checkbox" /></p>
+                </form>
             </div>
         </>
     );
 };
 
-export default Form;
+const mapStateToProps = (state) => {
+    return {
+        placeholder: state.toDoList.placeholder
+    };
+};
+const mapDispatchToProps = {
+    addItem,
+    reverse  
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Form);
